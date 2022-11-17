f834f8ae20743654702d45cb8081bb001032ee94
ekaras <key.cpp@gmail.com>
2022-11-17 23:58:17 +0300

    start

---
+++ UIButtonAndLabel/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  UIButtonAndLabel
+//
+//  Created by Евгений Карась on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet var actionButtons: [UIButton]!
+    
+    @IBAction func pressedBurron(_ sender: UIButton) {
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.titleLabel == "Action 1" {
+            label.text = "Hello, World!"
+            label.textColor = .red
+        } else {
+            
+        }
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textColor = UIColor.red
+        
+        
+        button.isHidden = true
+        
+        for button in actionButtons {
+ 
+            button.setTitleColor(.blue, for: UIControl.State.normal)
+            button.backgroundColor = .green
+        }
+        
+        button.setTitle("Clear", for: UIControl.State.normal)
+        button.setTitleColor(.white, for: UIControl.State.normal)
+        button.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
